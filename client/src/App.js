1b6b3b73182d5a4620fd83593abf927cbcb64650
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-04-12 16:18:55 +0530

    Implement landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,40 @@
 import './App.css';
 
-function App() {
+const Logo = () => (
+  <div className="logo-container">
+    <div className="logo"> flytrap </div>
+    <div className="logo-subtitle">bug tracking made easy</div>
+  </div>
+)
+
+const Card = () => { 
+  return (
+    <div className="card">
+      <Input />
+      <Input />
+      <Button className="login" text="Log In" />
+      <Button className="demo" text="Demo Account" />
+    </div> 
+  ) 
+}
+
+const Input = () => {
+  return (
+    <input type="text" / >
+  )
+}
+
+const Button = ({ className, text }) => {
+  return (
+    <button className={className}>{text}</button>
+  )
+}
+
+const App = () => {
   return (
     <div className="App">
-      <h1>Hello from React!</h1>
+      <Logo />
+      <Card />
     </div>
   );
 }
