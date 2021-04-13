b9782437e09ffa4f48c9a64bb06daeb3c4bdf4ef
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-04-13 20:04:56 +0530

    Extract components to separate files

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,11 @@
-import './App.css';
+import './App.scss';
+import Card from './Card';
 
 const Logo = () => (
   <div className="logo-container">
     <div className="logo"> flytrap </div>
-    <div className="logo-subtitle">bug tracking made easy</div>
   </div>
 )
-
-const Card = () => { 
-  return (
-    <div className="card">
-      <Input />
-      <Input />
-      <Button className="login" text="Log In" />
-      <Button className="demo" text="Demo Account" />
-    </div> 
-  ) 
-}
-
-const Input = () => {
-  return (
-    <input type="text" / >
-  )
-}
-
-const Button = ({ className, text }) => {
-  return (
-    <button className={className}>{text}</button>
-  )
-}
 
 const App = () => {
   return (
