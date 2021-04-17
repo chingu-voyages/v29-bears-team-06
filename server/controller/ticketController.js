8eae802fd9fa74e12643e1112582a7f29112044d
Kevin Chhan <kvnchhan@gmail.com>
2021-04-17 12:14:31 -0700

    Add update method to ticketController

--- server/controller/ticketController.js
+++ server/controller/ticketController.js
@@ -108,7 +108,89 @@ exports.createNewTicket = [
 	},
 ];
 
-exports.updateTicket = async (req, res, next) => {};
+exports.updateTicket = [
+	// validate
+	body("title")
+		.not()
+		.isEmpty()
+		.withMessage("Title field must not be empty")
+		.trim(),
+	body("project_id")
+		.not()
+		.isEmpty()
+		.withMessage("Project_id field must not be empty")
+		.trim()
+		.isNumeric()
+		.withMessage("Project_id field must be a number"),
+	body("developer_id")
+		.not()
+		.isEmpty()
+		.withMessage("Developer_id field must not be empty")
+		.trim()
+		.isNumeric()
+		.withMessage("Developer_id field must be a number"),
+	body("description")
+		.not()
+		.isEmpty()
+		.withMessage("Description field must not be empty")
+		.trim(),
+	body("type")
+		.not()
+		.isEmpty()
+		.withMessage("Type field must not be empty")
+		.isString()
+		.withMessage("Type field must be a string"),
+	body("priority")
+		.not()
+		.isEmpty()
+		.withMessage("Priority field must not be empty")
+		.isString()
+		.withMessage("Priority field must be a string"),
+	body("status")
+		.not()
+		.isEmpty()
+		.withMessage("Status field must not be empty")
+		.isString()
+		.withMessage("Status field must be a string"),
+
+	// sanitize
+	body("*").escape(),
+
+	async (req, res, next) => {
+		// express-validator result
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			// There are errors, send error message to client
+			return res.json({ errors: errors.array() });
+		} else {
+			// No errors. create new ticket
+			const {
+				title,
+				project_id,
+				developer_id,
+				description,
+				type,
+				priority,
+				status,
+				due_date,
+			} = req.body;
+
+			await models.Ticket.update({
+				title: title,
+				project_id: project_id,
+				developer_id: developer_id,
+				description: description,
+				type: type,
+				priority: priority,
+				status: status,
+				due_date: due_date,
+			});
+
+			return res.status(200);
+		}
+	},
+];
 
 exports.updateTicketById = async (req, res, next) => {};
 
