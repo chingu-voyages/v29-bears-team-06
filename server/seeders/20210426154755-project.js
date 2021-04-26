0d31658f36c60fad84db275500711a285795dfcc
Kevin Chhan <kvnchhan@gmail.com>
2021-04-26 09:34:45 -0700

    Add project migrations and seeeders

---
+++ server/seeders/20210426154755-project.js
@@ -0,0 +1,42 @@
+"use strict";
+
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		/**
+		 * Add seed commands here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkInsert('People', [{
+		 *   name: 'John Doe',
+		 *   isBetaMember: false
+		 * }], {});
+		 */
+
+		await queryInterface.bulkInsert(
+			"Projects",
+			[
+				{
+					title: "First Project",
+					project_id: 1,
+					developer_id: 1,
+					description: "This is a new Project",
+					priority: "high",
+					status: "in_progress",
+					due_date: new Date(),
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+			],
+			{}
+		);
+	},
+
+	down: async (queryInterface, Sequelize) => {
+		/**
+		 * Add commands to revert seed here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkDelete('People', null, {});
+		 */
+	},
+};
