652ae9f0c74a3bc373eb143e609f4ec2f10046e0
Kevin Chhan <kvnchhan@gmail.com>
2021-04-26 09:34:45 -0700

    Add project migrations and seeeders

---
+++ server/migrations/20210426154151-create-project.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Projects', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      manager_id: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      priority: {
+				type: Sequelize.STRING,
+				allowNull: true,
+			},
+			status: {
+				type: Sequelize.STRING,
+				allowNull: true,
+			},
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Projects');
+  }
+};
